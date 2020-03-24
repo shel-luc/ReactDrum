72b138747cf00c6f15c953092c7674981e103e2e
shel-luc <51696132+shel-luc@users.noreply.github.com>
2020-03-24 19:11:23 -0400

    Updates

---
+++ precache-manifest.13d435c53ee4adb27135a86b3491730a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6ed350ba96e24ac1986a332a8425e69",
+    "url": "/ReactDrum/index.html"
+  },
+  {
+    "revision": "e9bc33de96b6026aa2ab",
+    "url": "/ReactDrum/static/css/main.1f0bda95.chunk.css"
+  },
+  {
+    "revision": "baba7c1daa3fff55b29a",
+    "url": "/ReactDrum/static/js/2.f2750520.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ReactDrum/static/js/2.f2750520.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e9bc33de96b6026aa2ab",
+    "url": "/ReactDrum/static/js/main.3e0d6c28.chunk.js"
+  },
+  {
+    "revision": "772fa67af45b1760944c",
+    "url": "/ReactDrum/static/js/runtime-main.754de9df.js"
+  }
+]);
